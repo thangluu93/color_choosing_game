1d98f7ad2b704a1b9337ce87ddcacf415d035590
thangluu93 <luuthang9300@gmail.com>
2020-05-26 10:55:35 +0700

    asdsad

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,31 +11,66 @@ class HomePage extends StatelessWidget {
 
   Widget _buildSquare(BuildContext context) {
     return Container(
-      height: 300,
-      // height: MediaQuery.of(context).size.width,
-      child: GridView.count(
-        physics: ScrollPhysics(),
-        padding: EdgeInsets.only(
-          bottom: 75,
-          left: 75,
-          right: 75,
-          top: 40,
-        ),
-        crossAxisCount: 3,
-        children: List.generate(
-          3 * 3,
-          (index) => _buildColorBox(index, context),
-        ),
-      ),
-      // constraints: BoxConstraints(maxHeight: 100),
-    );
+        margin: EdgeInsets.only(top: 50),
+        // height: 300,
+        // height: MediaQuery.of(context).size.width-70,
+        height: 250,
+        width: 320,
+        // width: MediaQuery.of(context).size.width-70,
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              SizedBox(
+                height: MediaQuery.of(context).size.height / 9,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    _buildColorBox(0, context),
+                    _buildColorBox(1, context),
+                    _buildColorBox(2, context),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height / 9,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    _buildColorBox(3, context),
+                    _buildColorBox(4, context),
+                    _buildColorBox(5, context),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height / 9,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    _buildColorBox(6, context),
+                    _buildColorBox(7, context),
+                    _buildColorBox(8, context),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        )
+        // constraints: BoxConstraints(maxHeight: 100),
+        );
   }
 
   Widget _buildColorBox(int index, BuildContext context) {
     return Padding(
-      padding: EdgeInsets.all(7),
+      padding: EdgeInsets.all(0),
+
       child: Container(
-        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width / 4,
         decoration: BoxDecoration(
           color: index == 4 ? Colors.red : Colors.white,
           shape: BoxShape.circle,
